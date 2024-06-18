2b89135525511d9f264b64cfbda78cf7c455135d
Belal Ahmed Siddiqui <belalahmedsiiddiqui8@gmail.com>
2024-06-18 18:05:31 +0530

    COMMIT

--- pages/page_2.py
+++ pages/page_2.py
@@ -19,8 +19,8 @@ def main():
     if selected_material == "RMMR TABLE":
         st.link_button("DOWNLOAD RMMR TABLE📥", url="https://drive.google.com/file/d/1GxYgJQlY4sl6XDwmna01JFAVojprTuF5/view?usp=drive_link")
     elif selected_material == "QMS":
-        st.link_button("Download QMS STUDY MATERIAL 📖", url="https://drive.google.com/drive/folders/1AYiY3CMUkNWluAnP9BhMmZkx5RRZciwD?usp=sharing")
-        st.link_button("Download QMS ASSIGNMENT 📝", url="https://drive.google.com/drive/folders/1oWbDYyQ_ExKCuGF-B7aWfkh68MlCYllM?usp=sharing")
+        st.link_button("DOWNLOAD QMS STUDY MATERIAL 📖", url="https://drive.google.com/drive/folders/1AYiY3CMUkNWluAnP9BhMmZkx5RRZciwD?usp=sharing")
+        st.link_button("DOWNLOAD QMS ASSIGNMENT 📝", url="https://drive.google.com/drive/folders/1oWbDYyQ_ExKCuGF-B7aWfkh68MlCYllM?usp=sharing")
     elif selected_material == "EXAMINATION PAPERS":
         st.link_button("DOWNLOAD PAPERS[OLD] 📄", url="https://drive.google.com/drive/folders/1OU3Q5o9s0GYnd54FA3mV-ZDmTt32bX0e?usp=sharing")
         st.link_button("DOWNLOAD MID SEM PAPERS[NEW]📊🤓",url="https://drive.google.com/file/d/1mRHT-HpOKgi-Aj--hniZX7eStJO5Ghu1/view?usp=sharing")
