15fa5686e90f3eac4c504e8b509c7d85c3787aa2
Belal Ahmed Siddiqui <belalahmedsiiddiqui8@gmail.com>
2024-12-26 19:02:39 +0530

    Update Resume Building💻[UNDER DEVELOPEMENT].py

--- pages/Resume Building💻[UNDER DEVELOPEMENT].py
+++ pages/Resume Building💻[UNDER DEVELOPEMENT].py
@@ -20,6 +20,7 @@ def main():
     st.write("NOTE: Better to make resume here and download the latex code and upload the latex code in Overleaf to get the PDF.")
     st.write("Later add the ISI Kolkata LOGO using some PDF Editor Tool.")
     st.link_button("VISIT HACKERRESUME WEBSITE",url="https://www.hackerrank.com/resume")
+    st.link_button("VISIT PDF EDITOR TOOL",url="https://smallpdf.com/edit-pdf")
     st.subheader("Overleaf Template") 
     st.write('''This template is quite good,just edit this template using overleaf,it has all the required section
              that constitutes a GOOD Resume''')
