7324ce734f78cb5c5d5aa81264a1d53d5920adf9
Belal Ahmed Siddiqui <belalahmedsiiddiqui8@gmail.com>
2025-08-16 21:16:18 +0530

    Add Semester 3 study material page and remove old draft
    Introduced a new Streamlit page for Semester 3 study materials with subject selection and download links. Removed the previous under-development draft file.

---
+++ pages/Semester 3 📚.py
@@ -0,0 +1,50 @@
+import streamlit as st
+def main():
+    st.set_page_config(page_title="ISI QROR MATERIAL HUB", page_icon="📚", layout="centered", initial_sidebar_state="auto", menu_items=None)
+    st.title("Third Semester Study Material📚")
+    st.sidebar.success("SELECT THE PAGE ABOVE")
+    st.write("***PLEASE SELECT THE SUBJECT*** 🤓")
+
+   
+    
+  
+    selected_material = st.selectbox("Select the Subjects",[
+    "OPERATIONS RESEARCH",
+    "INDUSTRIAL EXPERIMENTATION",
+    "RELIABILITY - II",
+    "ADVANCED MULTIVARIATE ANALYSIS",
+    "BUSINESS ANALYTICS",
+    "SIX SIGMA",
+    "EXAMINATION PAPERS","RMMR TABLE"])
+    
+    if selected_material=="EXAMINATION PAPERS":
+        
+        st.link_button("DOWNLOAD MID SEM PAPERS[NEW]📚",url="https://drive.google.com/file/d/1vuO_BLW8CaQOG0WP1QD6yXArQ08g9IKF/view?usp=sharing")
+        st.link_button("DOWNLOAD END SEM PAPERS[NEW]🤓",url="https://drive.google.com/file/d/1GYFlYqzq3_i_-8ip6HQz20TVctEdXx_7/view?usp=sharing")
+    elif selected_material=="SIX SIGMA":
+        st.link_button("DOWNLOAD SIX SIGMA MATERIAL😎",url="")
+    
+
+    elif selected_material=="BUSINESS ANALYTICS":
+        st.link_button("DOWNLOAD BUSINESS ANALYTICS MATERIAL💻",url ="")
+    
+    elif selected_material=="ADVANCED MULTIVARIATE ANALYSIS":
+        st.link_button("DOWNLOAD AMA MATERIAL📒",url="")
+    
+    elif selected_material=="RELIABILITY - II":
+        st.link_button("DOWNLOAD RELIABILITY MATERIAL📊",url="")
+
+    elif selected_material=="INDUSTRIAL EXPERIMENTATION":
+        st.link_button("DOWNLOAD IE MATERIAL💻",url="")
+    
+    elif selected_material=="OPERATIONS RESEARCH":
+        st.link_button("DOWNLOAD OR MATERIAL📉",url= "")
+    
+
+    elif selected_material=="RMMR TABLE":
+        st.link_button("DOWNLOAD RMMR TABLE📥",url="https://drive.google.com/file/d/1GxYgJQlY4sl6XDwmna01JFAVojprTuF5/view?usp=drive_link")
+    
+
+    
+if __name__ == "__main__":
+    main()
