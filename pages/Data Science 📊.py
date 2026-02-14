11cd25a0c83810b226762591ae087bf44f3fa8b9
Belal Ahmed Siddiqui <belalahmedsiiddiqui8@gmail.com>
2026-02-14 17:36:11 +0530

    added interview platform

--- pages/Data Science 📊.py
+++ pages/Data Science 📊.py
@@ -6,9 +6,11 @@ def main():
     st.image("https://media.licdn.com/dms/image/C4D12AQGD_su1k14bYA/article-cover_image-shrink_720_1280/0/1583217310732?e=2147483647&v=beta&t=an15cumXvL1rLYucw3WqOUkf-27-B-l52jkmpEfPaKw")
     st.sidebar.success("SELECT THE PAGE ABOVE")
  
-    selected_material = st.selectbox("Select the Options", ["YT PLAYLIST","PROJECT","ML BOOKS"])
+    selected_material = st.selectbox("Select the Options", ["INTERVIEW","YT PLAYLIST","PROJECT","ML BOOKS"])
     if selected_material=="ML BOOKS":
             st.link_button("DOWNLOAD DATA SCIENCE BOOKS[COMPILED BY SOUBHIK BHATTACHARYA]⚡⚡",url="https://drive.google.com/drive/folders/1tMlOwyXzpiiSWmvlP-X7HSrc4dxBi0YJ?usp=sharing")
+    elif selected_material=="INTERVIEW":
+        st.link_button("INTERVIEW PREP PLATFORM",url="https://cracked-ds-platform.vercel.app")
             
     elif selected_material=="YT PLAYLIST":
 
