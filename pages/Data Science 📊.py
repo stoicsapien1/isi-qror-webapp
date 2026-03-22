07e091093f6c19be009834067e3d58687ac52533
Belal Ahmed Siddiqui <belalahmedsiiddiqui8@gmail.com>
2026-03-22 10:01:43 +0530

    removed images st.image

--- pages/Data Science 📊.py
+++ pages/Data Science 📊.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="ISI QROR MATERIAL HUB", page_icon="📚", layout="centered", initial_sidebar_state="auto", menu_items=None)
     st.title("Data Science Study Material📚")
-    st.image("https://media.licdn.com/dms/image/C4D12AQGD_su1k14bYA/article-cover_image-shrink_720_1280/0/1583217310732?e=2147483647&v=beta&t=an15cumXvL1rLYucw3WqOUkf-27-B-l52jkmpEfPaKw")
+    
     st.sidebar.success("SELECT THE PAGE ABOVE")
  
     selected_material = st.selectbox("Select the Options", ["INTERVIEW","YT PLAYLIST","PROJECT","ML BOOKS"])
